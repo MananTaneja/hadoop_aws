ac164201ac930833ceb4008cba7c83555e93b215
manantaneja <manantaneja007@gmail.com>
2021-01-12 13:16:43 +0530

    updated csv and made commands

---
+++ hive.sql
@@ -0,0 +1,20 @@
+create table food(
+    adm0_id INT ,
+    adm0_name VARCHAR(100),
+    adm1_id INT,
+    adm1_name VARCHAR(100),
+    mkt_id INT,
+    mkt_name VARCHAR(100),
+    cm_id INT,
+    cm_name VARCHAR(100),
+    cur_id INT,
+    cur_name VARCHAR(100),
+    pt_id INT,
+    pt_name VARCHAR(100),
+    noofunits INT,
+    um_name VARCHAR(100),
+    mp_month INT,
+    mp_year INT,
+    mp_price INT,
+    mp_commoditysource VARCHAR(100)
+)
