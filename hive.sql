7e9234ee7d8ec82845d8abfb5b64e94440cd2f0d
manantaneja <manantaneja007@gmail.com>
2021-01-14 04:08:52 +0530

    updates to commands

--- hive.sql
+++ hive.sql
@@ -1,3 +1,5 @@
+-- Create table and load data
+
 create table food(
     adm0_id INT ,
     adm0_name VARCHAR(100),
@@ -17,4 +19,21 @@ create table food(
     mp_year INT,
     mp_price INT,
     mp_commoditysource VARCHAR(100)
-)
+) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE TBLPROPERTIES("skip.header.line.count"="1");
+
+LOAD DATA LOCAL INPATH '/home/hduser/hadoop_aws/food.csv' INTO TABLE food;
+
+
+-- Select query
+select * from food;
+
+-- Aggregatio Query
+select count(adm0_id) from food where cm_name="Bread";
+select count(adm0_id) from food where cm_name="Wheat";
+
+-- Group by
+select cm_name, count(adm0_id) from food group by cm_name;
+
+-- subquery
+
+select max(mp_price) from food where mp_year in(select max(mp_year) from food);
